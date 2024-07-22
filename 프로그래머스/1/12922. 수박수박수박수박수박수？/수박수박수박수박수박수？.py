fe81c690b3e989076ce5f21224e2219d2aeaeb2e
yongwuk <140808035+dnslfkrh@users.noreply.github.com>
2024-07-23 00:37:46 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.42 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = ''
+    if n % 2 == 1:
+        for i in range((n-1) // 2):
+            answer += "수박"
+        answer += "수"
+    else:
+        for i in range(n // 2):
+            answer += "수박"
+        
+    return answer
