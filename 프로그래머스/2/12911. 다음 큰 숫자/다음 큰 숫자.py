1b0fc7f256f6ac3a932796df11dde8bbffcfa361
yonguk <lyw3059@gmail.com>
2024-10-15 21:51:53 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,10 +1,8 @@
 def solution(n):
     
-    binN = bin(n)[2:]
     countN = bin(n).count("1")
     
     for i in range(n+1, n+1000000):
-        binI = bin(i)[2:]
         countI = bin(i).count("1")
         
         if countN == countI:
